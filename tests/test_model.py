a8589b7bfba7046cc3afdb8be480e64e5e0c8c8b
Sarah Bull <sarah.bull@balliol.ox.ac.uk>
2023-10-19 14:46:49 +0100

    final testing code

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import unittest
 import pkmodel as pk
+import numpy as np
 
 class ModelTest(unittest.TestCase):
         """
@@ -12,38 +13,45 @@ class ModelTest(unittest.TestCase):
                 model = pk.Model()
                 self.assertEqual(model.value, 42)
                 self.assertIsInstance(model, pk.Model, 'Error initiated wrong class')
+                
         
-        def test_dose(self):
-                model = pk.Model
-                t = 2
-                X = 5
-                result = model.dose(t, X)
-                self.assertEqual(result, 5)
-                assert isinstance(result, int), 'Error, output not correct object type'
+                
         
-        def test_param(self):
+        def test_paramIV(self):
                 """
                 Tests output of IV function
                 """
                 model = pk.Model
                 t = np.array(0, 1, 1000)
                 y = np.array([1, 1]) 
-                Q_p1 = 2
-                V_c = 2
-                V_p1 = 4
-                CL = 2
-                X = 1
+                
+                self.parameters = [2, 2, 4, 2, 1]
+        
  
-                output = model.IV(t, y, Q_p1, V_c, V_p1, CL, X)
+                output = model.paramIV(y, t)
 
                 self.assertEqual(output[1], 0.5)
-                self.assertEqual(output[0], )
-                assert isinstance(output[1], float), 'Error, output not correct object type'
-                assert isinstance(output[0], float), 'Error, output not correct object type'
+                self.assertEqual(output[0], 0.5)
+                self.assertIsInstance(output[1], float), 'Error, output not correct object type'
+                self.assertIsInstance(output[0], float), 'Error, output not correct object type'
                    
                 
-        #def test_SC(self):
-               # model = pk.Model
-
-#Length args = length reqiured for the models
-#Test variables required for the models 
+        def test_paramSC(self):
+                """
+                Tests output of SC function
+                """
+                model = pk.Model
+                self.parameters = [2, 2, 4, 2, 1, 2]
+                y = [1, 1, 1]
+                t = np.array(0, 1, 1000)
+                
+                output = model.paramSC(y, t)
+                
+                self.assertEqual(output[0], 0.5)
+                self.assertEqual(output[1], 0.5)
+                self.assertEqual(output[2], -1)
+                
+                self.assertIsInstance(output[0], float), 'Error, output not correct object type'
+                self.assertIsInstance(output[1], float), 'Error, output not correct object type'
+                self.assertIsInstance(output[2], float), 'Error, output not correct object type'  
+                         
